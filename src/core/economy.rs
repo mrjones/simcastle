5bb8ee1c49ffdac4f5adf4f20bc8bfd00671e9c3
Matt Jones <jonesmr@gmail.com>
2020-05-25 18:27:43 +0000

    factor out cotenure model

--- src/core/economy.rs
+++ src/core/economy.rs
@@ -4,30 +4,58 @@ use super::population;
 use super::team;
 use super::types;
 
-pub struct SkillModel {
-    weights: std::collections::HashMap<character::Trait, f32>,
+struct SkillModel {
+    linear_weights: std::collections::HashMap<character::Trait, f32>,
 }
 
 impl SkillModel {
-    pub fn new(weights: std::collections::HashMap<character::Trait, f32>) -> SkillModel {
-        return SkillModel{weights: weights};
+    fn new(linear_weights: std::collections::HashMap<character::Trait, f32>) -> SkillModel {
+        return SkillModel{linear_weights: linear_weights};
     }
 
-    pub fn character_skill_boost(&self, c: &character::Character) -> f32 {
+    fn character_skill_boost(&self, c: &character::Character) -> f32 {
         let mut skill_stdevs = 0.0;
-        for (t, weight) in &self.weights {
+        for (t, weight) in &self.linear_weights {
             skill_stdevs += weight * (c.get_trait(*t) as f32 - 50.0) / 10.0;
         }
         return skill_stdevs;
     }
 
-    pub fn team_skill_boost(&self, team: &team::Team, population: &population::Population) -> f32 {
+    fn team_skill_boost(&self, team: &team::Team, population: &population::Population) -> f32 {
         let mut skills_boost = 0.0;
         for id in team.members() {
             let c = population.character_with_id(id.clone()).expect("food_production::character_with_id");
             skills_boost += self.character_skill_boost(c);
         }
         return skills_boost;
+    }
+}
+
+struct CotenureModel {
+    log_base: f32,
+    multiplier: f32,
+}
+
+impl CotenureModel {
+    pub fn boost(&self, team: &team::Team, rapport_tracker: &population::RapportTracker) -> f32 {
+        if team.members().len() < 2 {
+            return 1.0;
+        }
+
+        let mut total_cotenure: i32 = 0;
+        let mut num_pairs: i32 = 0;
+        for (c1, c2) in team.member_pairs() {
+            total_cotenure += rapport_tracker.turns_on_same_team(&c1, &c2);
+            num_pairs += 1;
+        }
+
+        if total_cotenure == 0 {
+            return 1.0;
+        }
+
+        let average_cotenure: f32 = total_cotenure as f32 / num_pairs as f32;
+
+        return 1.0 + (average_cotenure.log(self.log_base) * self.multiplier);
     }
 }
 
@@ -43,6 +71,7 @@ pub struct FoodEconomy {
 }
 
 pub fn food(farmers: &team::Team, food_infrastructure: &castle::FoodInfrastructure, population: &population::Population) -> FoodEconomy {
+    // TODO(mrjones): Consider sublinear (rather than 0) growth once 'farmers > acres of farmland'
     let base_production: f32 =
         std::cmp::min(food_infrastructure.acres_of_farmland, farmers.members().len() as i32) as f32;
     let mut production: f32 = base_production;
@@ -51,25 +80,15 @@ pub fn food(farmers: &team::Team, food_infrastructure: &castle::FoodInfrastructu
         character::Trait::Intelligence => 0.05,
         character::Trait::WorkEthic => 0.1,
     });
+    let cotenure_model = CotenureModel{
+        log_base: 100.0,
+        multiplier: 1.0 / 3.0,
+    };
 
     let skills_boost = skill_model.team_skill_boost(farmers, population);
     production = production + skills_boost;
 
-    let mut cotenure_boost = 1.0;
-    if farmers.members().len() > 1 {
-        let mut total_cotenure: i32 = 0;
-        let mut num_pairs: i32 = 0;
-        for (c1, c2) in farmers.member_pairs() {
-            total_cotenure += population.rapport_tracker().turns_on_same_team(&c1, &c2);
-            num_pairs += 1;
-        }
-
-        let average_cotenure: f32 = total_cotenure as f32 / num_pairs as f32;
-
-        if average_cotenure > 0.0 {
-            cotenure_boost = 1.0 + (average_cotenure.log(100.0) / 3.0);
-        }
-    }
+    let cotenure_boost = cotenure_model.boost(farmers, population.rapport_tracker());
     production *= cotenure_boost;
 
     return FoodEconomy{
